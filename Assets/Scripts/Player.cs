a79662b90930b533971c69ae4f1ce98ec90582ca
krakzy <yannick.vos987@gmail.com>
2023-12-03 14:28:07 +0100

    power ups codes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,14 @@
+using System.Collections;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public PlayerSpriteRenderer smallRenderer;
     public PlayerSpriteRenderer bigRenderer;
+    private PlayerSpriteRenderer activeRenderer;
 
     private DeathAnimation deathAnimation;
+    private CapsuleCollider2D capsuleCollider;
 
     public bool big => bigRenderer.enabled;
     public bool small => smallRenderer.enabled;
@@ -14,6 +17,7 @@ public class Player : MonoBehaviour
     private void Awake()
     {
         deathAnimation = GetComponent<DeathAnimation>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
     public void Hit()
@@ -28,11 +32,6 @@ public class Player : MonoBehaviour
         }
     }
 
-    private void Shrink()
-    {
-        // TODO
-    }
-
 
     private void Death()
     {
@@ -41,5 +40,53 @@ public class Player : MonoBehaviour
         deathAnimation.enabled = false;
 
         GameManager.Instance.ResetLevel(3f);
+    }
+
+    public void Grow()
+    {
+        smallRenderer.enabled = false;
+        bigRenderer.enabled = true;
+        activeRenderer = bigRenderer;
+
+        capsuleCollider.size = new Vector2(1f, 2f);
+        capsuleCollider.offset = new Vector2(0f, 0.5f);
+
+        StartCoroutine(ScaleAnimation());
+    }
+
+    private void Shrink()
+    {
+        smallRenderer.enabled = true;
+        bigRenderer.enabled = false;
+        activeRenderer = smallRenderer;
+
+        capsuleCollider.size = new Vector2(1f, 1f);
+        capsuleCollider.offset = new Vector2(0f, 0f);
+
+        StartCoroutine(ScaleAnimation());
+
+    }
+
+    private IEnumerator ScaleAnimation()
+    {
+        float elapsed = 0f;
+        float duration = 0.5f;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+
+            if (Time.frameCount % 4 == 0)
+            {
+                smallRenderer.enabled = !smallRenderer.enabled;
+                bigRenderer.enabled = !smallRenderer.enabled;
+            }
+
+            yield return null;
+        }
+
+        smallRenderer.enabled = false;
+        bigRenderer.enabled = false;
+        activeRenderer.enabled = true;
     }
 }
--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PowerUp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum Type
     {
-        
+        Coin,
+        ExtraLife,
+        MagicMushroom,
     }
 
-    // Update is called once per frame
-    void Update()
+    public Type type;
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            Collect(other.gameObject);
+        }
     }
+
+    private void Collect(GameObject player)
+    {
+        switch (type)
+        {
+            case Type.Coin:
+                GameManager.Instance.AddCoin();
+                break;
+
+            case Type.ExtraLife:
+                GameManager.Instance.AddLife();
+                break;
+
+            case Type.MagicMushroom:
+                player.GetComponent<Player>().Grow();
+                break;
+        }
+
+        Destroy(gameObject);
+    }
+
 }
